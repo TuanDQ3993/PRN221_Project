425c12acc125eadd76d7ae82e3f6ace3c05eff29
Hoàng Phi Hồng <honghphe176041@fpt.edu.vn>
2024-10-21 15:59:12 +0700

    them search và filter cho user va author

--- ProjectPRN221_GroupJQK/ManageAuthor.xaml.cs
+++ ProjectPRN221_GroupJQK/ManageAuthor.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectPRN221_GroupJQK.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,39 @@ namespace ProjectPRN221_GroupJQK
         public ManageAuthor()
         {
             InitializeComponent();
+        }
+        string[] elements = { "Name" };
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            load();
+            cbSearchBy.ItemsSource = elements;
+            cbSearchBy.SelectedIndex = 0;
+        }
+
+        private void load()
+        {
+            var authors = PRN221_LibContext.Ins.Authors.Select(x => new
+            {
+                AuthorId=x.AuthorId,
+                AuthorName=x.AuthorName,
+            }).ToList();
+            dgvAuthor.ItemsSource = authors;
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string keyword = txtSearch.Text;
+            string searchBy = cbSearchBy.SelectedItem.ToString();
+
+            var query = PRN221_LibContext.Ins.Authors.AsQueryable();
+            if (searchBy == "Name")
+            {
+                query = query.Where(author => author.AuthorName.Contains(keyword));
+            }
+
+
+            // Hiển thị kết quả tìm kiếm
+            dgvAuthor.ItemsSource = query.ToList();
         }
     }
 }
--- ProjectPRN221_GroupJQK/ManageUser.xaml.cs
+++ ProjectPRN221_GroupJQK/ManageUser.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectPRN221_GroupJQK.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,9 +27,66 @@ namespace ProjectPRN221_GroupJQK
             InitializeComponent();
         }
 
+        string[] elements = { "Name", "Email", "Phone", "Address" };
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
+            load();
+            cbSearchBy.ItemsSource = elements;
+            cbSearchBy.SelectedIndex = 0;
+            loadRole();
+        }
 
+        private void load()
+        {
+            var users = PRN221_LibContext.Ins.Users.Include(u=>u.RoleNavigation).ToList();
+            dgvUser.ItemsSource = users;
+        }
+        private void loadRole()
+        {
+            var role = PRN221_LibContext.Ins.Roles.Select(r => r.RoleName).ToList();
+            cbRole.ItemsSource = role;
+            role.Insert(0, "All");
+            cbRole.SelectedIndex = 0;
+
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string keyword = txtSearch.Text;
+            string searchBy = cbSearchBy.SelectedItem.ToString();
+
+            var query = PRN221_LibContext.Ins.Users.AsQueryable();
+            if (searchBy == "Name")
+            {
+                query = query.Where(user => user.FullName.Contains(keyword));
+            }
+            else if (searchBy == "Email")
+            {
+                query = query.Where(user => user.Email.Contains(keyword));
+            }
+            else if (searchBy == "Phone")
+            {
+                query = query.Where(user => user.PhoneNumber.Contains(keyword));
+            }
+            else if (searchBy == "Address")
+            {
+                query = query.Where(user => user.Address.Contains(keyword));
+            }
+
+            // Hiển thị kết quả tìm kiếm
+            dgvUser.ItemsSource = query.ToList();
+        }
+
+        private void cbRole_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedRole = cbRole.SelectedItem.ToString();
+            var query = PRN221_LibContext.Ins.Users.AsQueryable();
+
+            if (selectedRole != "All")
+            {
+                query = query.Where(user => user.RoleNavigation.RoleName == selectedRole);
+            }
+            dgvUser.ItemsSource = query.ToList();
         }
     }
 }
